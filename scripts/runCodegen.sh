613e8ad09d6872b6ba5cd11ef2870974a71f4860
Frank Buonarota <fbuonaro@gmail.com>
2022-03-24 00:31:58 -0400

    update codegen paths

--- scripts/runCodegen.sh
+++ scripts/runCodegen.sh
@@ -1,11 +1,11 @@
-docker run -v ${PWD}/liblhwsmodelapi/resource/swagger:/lhwsmodelapi --rm lhmodel-scg:main \
-  -i /lhwsmodelapi/LHWSModelApi.private.json \
-  -c /lhwsmodelapi/LHWSModelApi.private.config.json \
+docker run -v ${PWD}/liblhwsmodelapi/resource/swagger/lhwsmodelapi:/lhwsmodelapi --rm lhmodel-scg:main \
+  -i /lhwsmodelapi/LHWSModelApi.json \
+  -c /lhwsmodelapi/LHWSModelApi.config.json \
   --additional-properties \
     modelIncludeDir=lhwsmodelapi,\
     namespace=LHWSModelApiNS,\
     lcModelFilename=true,\
   -o /lhwsmodelapi \
   -v
-cp ${PWD}/liblhwsmodelapi/resource/swagger/src/io/swagger/server/model/*.h ${PWD}/liblhwsmodelapi/include/lhwsmodelapi/lhwsmodelapi/
-cp ${PWD}/liblhwsmodelapi/resource/swagger/src/io/swagger/server/model/*.cxx ${PWD}/liblhwsmodelapi/src/
+cp ${PWD}/liblhwsmodelapi/resource/swagger/lhwsmodelapi/src/io/swagger/server/model/*.h ${PWD}/liblhwsmodelapi/include/lhwsmodelapi/lhwsmodelapi/
+cp ${PWD}/liblhwsmodelapi/resource/swagger/lhwsmodelapi/src/io/swagger/server/model/*.cxx ${PWD}/liblhwsmodelapi/src/
